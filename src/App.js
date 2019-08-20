168fdb605e0b691cab1c12af9ca5940148d35c80
James <ruiz.james12@yahoo.com>
2019-08-20 10:30:39 -0700

    Fetching api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './CardList'
+import SearchBox from './SearchBox'
 import {robots} from './robots'
 
-const App = () => {
-    return (
-        <div>
-            <h1>Robo Friends</h1>
-            <CardList robots={robots} />
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState( {robots: users }))
+    }
+
+    onSearchChange = e => {
+        this.setState({ searchField: e.target.value })
+    }
+
+    render () {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
 }
 
 export default App
